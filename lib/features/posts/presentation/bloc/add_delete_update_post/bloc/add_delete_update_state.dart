59412f6d1fd4b620cb100c5510c4d856e1f7a2e6
Loujein Bouzazi <loujein.bouzazi@digitrends.dev>
2023-02-20 15:54:57 +0100

    create data layer

--- lib/features/posts/presentation/bloc/add_delete_update_post/bloc/add_delete_update_state.dart
+++ lib/features/posts/presentation/bloc/add_delete_update_post/bloc/add_delete_update_state.dart
@@ -1,9 +1,10 @@
 part of 'add_delete_update_bloc.dart';
 
 abstract class AddDeleteUpdateState extends Equatable {
-  const AddDeleteUpdateState();  
-
+  const AddDeleteUpdateState();
+  
   @override
   List<Object> get props => [];
 }
+
 class AddDeleteUpdateInitial extends AddDeleteUpdateState {}
